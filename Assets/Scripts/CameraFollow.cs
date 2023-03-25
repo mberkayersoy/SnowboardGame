600f31f28fb9b386b03937e63ba082d3d69a3cee
23sent <buadalinmadi@gmail.com>
2023-03-26 00:13:57 +0300

    Added PhysicsController.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target; 
-    public float smoothSpeed = 0.125f; 
-    public Vector3 offset;
-    Vector3 velocity = Vector3.zero;
+  public Transform target;
+  public float smoothSpeed = 0.125f;
+  public Vector3 offset;
+  Vector3 velocity = Vector3.zero;
 
-    void FixedUpdate()
-    {
-        Vector3 desiredPosition = target.position + offset;  
-        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);  
-        transform.position = smoothedPosition; 
-        transform.LookAt(target);  
-    }
+  void FixedUpdate()
+  {
+    Vector3 desiredPosition = target.position + offset;
+    Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+    transform.position = smoothedPosition;
+    transform.LookAt(target);
+  }
 }
