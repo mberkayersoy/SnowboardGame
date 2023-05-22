836104de841d68e0d909e3a41cd6f28f0fd44def
23sent <buadalinmadi@gmail.com>
2023-05-22 22:04:49 +0300

    added greean area for obstacles

--- Assets/Scripts/Collectible/Obstacle.cs
+++ Assets/Scripts/Collectible/Obstacle.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    public GameObject greenArea;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) 
+        if (other.CompareTag("Player"))
         {
             Debug.Log("OnTriggerEnter");
             UIManager.Instance.UpdateScore(+1);
+            greenArea.SetActive(false);
         }
         if (other.CompareTag("IceFloor"))
         {
